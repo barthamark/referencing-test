51ff03f6fefde583420bdb1a008332e9b4d475de
Márk Bartha <barthamark@gmail.com>
2019-06-23 15:53:17 +0200

    Testing absolute URL link using markdown syntax

--- src/Program.cs
+++ src/Program.cs
@@ -13,3 +13,4 @@ namespace src
 
 // See [this file](Test.cs)
 // See https://github.com/barthamark/referencing-test/blob/master/src/Test.cs
+// See [this file](https://github.com/barthamark/referencing-test/blob/master/src/Test.cs)
